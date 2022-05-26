c94b371d8f7c8e3210f9ef4e584fe946ef905320
Abdullah Al Samad <smdshakibmia2001@gmail.com>
2022-05-26 20:17:57 +0600

    footer added

--- src/components/pages/Home/Footer/Footer.js
+++ src/components/pages/Home/Footer/Footer.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <div className="sticky bottom">this is footer</div>;
+  return (
+    <div className="text-center bg-green-400 py-5">
+      Manufacturer Website &copy; 2022. All Rights Reserved
+      <p>
+        Developed By{" "}
+        <Link to="/about-me" className="underline hover:no-underline">
+          Md. Shakib Mia
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -3,7 +3,6 @@ import Banner from "./Banner/Banner";
 import Tools from "./Tools/Tools";
 import Reviews from "./Reviews/Reviews";
 import Footer from "./Footer/Footer";
-import PlaceReview from "./PlaceReview/PlaceReview";
 import BusinessSummary from "./BusinessSummary";
 
 const Home = () => {
@@ -13,7 +12,6 @@ const Home = () => {
       <BusinessSummary></BusinessSummary>
       <Tools></Tools>
       <Reviews></Reviews>
-      <PlaceReview></PlaceReview>
       <Footer></Footer>
     </div>
   );
