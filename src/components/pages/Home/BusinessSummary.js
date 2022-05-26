1748371271189ae2d4025a1f67107e72e1dfbff1
Abdullah Al Samad <smdshakibmia2001@gmail.com>
2022-05-26 17:24:24 +0600

    business summary added

---
+++ src/components/pages/Home/BusinessSummary.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const BusinessSummary = () => {
+  return (
+    <div className="flex flex-wrap container mx-auto justify-around">
+      <div className="border-2 border-green-500 p-10 inline-block  text-center w-50 h-32 rounded-full">
+        <i className="fas fa-user text-2xl"></i>
+        <br />
+        100+ Customers
+      </div>
+      <div className="border-2 border-green-500 p-10 inline-block text-center w-50 h-32 rounded-full">
+        <i className="fa-solid fa-sack-dollar text-2xl"></i>
+        <br />
+        12M+ Annual Revenue
+      </div>
+      <div className="border-2 border-green-500 p-10 inline-block text-center w-50 h-32 rounded-full">
+        <div>
+          <i className="fa-solid fa-star-half"></i>
+          <i className="fa-solid fa-star text-xl"></i>
+          <i className="fa-solid fa-star-half-stroke"></i>
+        </div>
+        33K+ Reviews
+      </div>
+      <div className="border-2 border-green-500 p-10 inline-block text-center w-50 h-32 rounded-full">
+        <i class="fa-solid fa-screwdriver-wrench"></i>
+        <br />
+        50+ tools
+      </div>
+    </div>
+  );
+};
+
+export default BusinessSummary;
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -4,11 +4,13 @@ import Tools from "./Tools/Tools";
 import Reviews from "./Reviews/Reviews";
 import Footer from "./Footer/Footer";
 import PlaceReview from "./PlaceReview/PlaceReview";
+import BusinessSummary from "./BusinessSummary";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <BusinessSummary></BusinessSummary>
       <Tools></Tools>
       <Reviews></Reviews>
       <PlaceReview></PlaceReview>
